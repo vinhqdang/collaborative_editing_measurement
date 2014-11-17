656ab11ef4e588c251ef1fb72d760bc953949a95
Quang-Vinh Dang <quang-vinh.dang@inria.fr>
2014-11-17 11:44:55 +0100

    Add result processing script

---
+++ processResult.py
@@ -0,0 +1,68 @@
+import sys
+import matplotlib.pyplot as plt
+
+if __name__ == "__main__":
+	file_name = sys.argv [1]
+	file = open (file_name, "r")
+	lines = file.readlines ()
+	
+	#7 number_of_user: 2, 5, 10, 20, 30, 40, 50
+	#each record contain: type speed and delay
+	records = [[list() for x in xrange(2)] for x in xrange(50)] 
+	
+	test_cases = [[0 for x in xrange(5)] for x in xrange (100)]
+	counter = 0
+	for line in lines:
+		counter += 1
+		if ('***' in line):
+			counter = 0
+			test_cases = [[0 for x in xrange(5)] for x in xrange (100)]
+			continue
+		if ('---' in line):
+			continue
+		ls = line.split ()
+		#found writing record
+		if (ls[0] == 'W'):
+			n_user = int (ls[1])
+			type_spd = int (ls[2])
+			exp_id = int (ls[3])
+			string = int (ls[4])
+			w_time = int (ls[5])
+
+			test_cases[string][0] = n_user
+			test_cases[string][1] = type_spd
+			test_cases[string][2] = exp_id
+			test_cases[string][4] = w_time
+
+			#add to record
+			if (test_cases[string][3] != 0):
+				res = list ()
+				res.append (type_spd)
+				res.append (test_cases[string][3] - w_time)
+				records[n_user - 1].append (res)
+		#found reading record
+		if (ls[0] == 'R'):
+			n_user = int (ls[1])
+			type_spd = int (ls[2])
+			exp_id = int (ls[3])
+			string = int (ls[4])
+			r_time = int (ls[5])
+
+			#if (test_cases[string][0] == n_user && test_cases[string[1] == type_spd && test_cases[string[2]] = exp_id):
+			test_cases[string][3] = r_time
+	#test
+	#print 'TEST'
+	#for i in range (len (records [2])):
+	#	print records[2][i]
+	file.close ()
+
+	#write the result
+	file = open ("getResult.txt", "w")
+	us = [1, 2, 5, 10, 20, 30, 40, 50]
+	#print len (records)
+	file.write ("user speed delay\n")
+	for u in us:
+		for i in range (len (records[u - 1])):
+			if (len (records[u - 1][i]) == 2):
+				file.write (str(u) + " " + str(records[u-1][i][0]) + " " + str(records[u-1][i][1]) + "\n")
+	file.close
