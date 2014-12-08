bfb54f3935363d25f85789127d46ef468fb81d27
Vinh Q. DANG <dqvinh87@gmail.com>
2014-12-08 10:08:59 +0100

    Update Cryptpad

--- src/fr/inria/coast/cryptpad/Main.java
+++ src/fr/inria/coast/cryptpad/Main.java
@@ -15,32 +15,32 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int n_users[] = {1,2,5,10,15,20,25,30,40,50};
+		int n_users[] = {1,2,5,10,15,20,30,40,50};
 
 		// TODO fill the last experimental information here
-		int last_user = 20;
-		int last_type = 10;
-		int last_exp = 3;
+		int last_user = 25;
+		int last_type = 1;
+		int last_exp = 2;
 		
-		int doc_name = 2000;
+		int doc_name = 2103;
 
 		for (int i = 0; i < n_users.length; i++) {
 			int n_user = n_users [i];
 			for (int type_spd = 1; type_spd <= 10; type_spd++) {
 				if (type_spd % 2 != 0 && type_spd != 1) continue;
-				//if (n_user >= 40 && type_spd != 1) continue;
+				if (n_user >= 40 && type_spd != 1) continue;
 				for (int exp_id = 1; exp_id <= 3; exp_id += 1) {
 					//continue from last time
 					if (n_user < last_user || (n_user == last_user && type_spd < last_type || (n_user == last_user && type_spd == last_type && exp_id <= last_exp))) continue;
 
-					System.out.println ("Running Cryptpad: " + n_user + " " + type_spd + " " + exp_id);
+					System.out.println ("Running Cryptpad: " + n_user + " " + type_spd + " " + exp_id + " " + doc_name);
 
 					String DOC_URL = "http://152.81.3.91:3000/#" + Integer.toString(doc_name++);
 
 					CryptpadAutomator automator = new CryptpadAutomator(n_user, type_spd, exp_id, DOC_URL, 10, "cryptpad_deletion.txt");
 					automator. run();
 					try {
-						System.out.println("Finished cryptpad:" + n_user + " " + type_spd + " " + exp_id + "" + doc_name);
+						System.out.println("Finished cryptpad:" + n_user + " " + type_spd + " " + exp_id + " " + doc_name);
 						System.gc();
 						Thread.sleep(45000);
 					} catch (InterruptedException e) {
